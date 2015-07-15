41a2230ebcf52bc385808a56cc78931711c628c0
unknown <sidb235@LNIDPC003109D.wads.res>
2015-07-14 22:10:00 -0700

    moving on the api

--- mixins/api.js
+++ mixins/api.js
@@ -3,6 +3,9 @@ define(function (require) {
 
     var dispatcher = require('mixins/dispatcher');
 
+    //caching and queueing containers
+    FluxMarionette.api = FluxMarionette.api || { cache: {}, queue: {}};
+
     //this request method is how we get data from the API
     return function () {
         //mixins
@@ -19,44 +22,61 @@ define(function (require) {
                 return md5(url + action + JSON.stringify(payload));
             },
 
-            ajax: function(url, action, payload, cache) {
+            ajax: function(url, action, payload, doCache) {
                 //error handling
                 if(!url || !action) throw new Error('must pass url and action to the api functions');
 
                 //cache by default
-                cache = (typeof cache === "undefined") ? true : cache;
+                doCache = (typeof doCache === "undefined") ? true : doCache;
 
                 //get the data Id
-                var dataId = this.dataId(url, action, payload);
+                var self = this,
+                    dataId = this.dataId(url, action, payload);
 
-                //this lets things know we've sent the request out
-                this.dispatch("api:" + dataId + ":requested");
+                //is it in the queue alredy?
+                if(!FluxMarionette.api.que[dataId]){
+                    //add it to the queue
+                    FluxMarionette.api.que[dataId] = true;
 
-                //check the cache
-                if(FluxMarionette.cache.dataId && cache){
+                    //this lets things know we've sent the request out
+                    this.dispatch("api:" + dataId + ":requested");
 
-                } else {
-                    
+                    //check the cache
+                    var promise;
+                    if(FluxMarionette.api.cache[dataId] && doCache){
+                        promise = $.Deferred().resolve(FluxMarionette.api.cache[dataId]);
+                    } else {
+                        
+                        promise = $.ajax({
+                            type: action,
+                            url: url,
+                            data: JSON.stringify(payload),
+                            contentType: 'application/json; charset=utf-8',
+                            dataType: 'json'
+                        });
+                    }
+
+                    promise
+                    .done(function (data) {
+                        //dispatch the message, this dispatch is identified by the request params
+                        this.dispatch("api:" + dataId + ":received", data);
+                        
+                        //remove this call from the sync cue
+                        delete FluxMarionette.api.que[dataId];
+
+                        //cache the data
+                        if (doCache) {
+                            FluxMarionette.api.cache[dataId] = data;
+                        }
+                    })
+                    .fail(function () {
+                        //do some logging *********************************************
+
+                        //remove this call from the sync cue
+                        delete FluxMarionette.api.que[dataId];
+                    });
                 }
-
-                //add to que
-
-                //get the data
-
-                //add data to cache
-
-
-                /*
-                //create a promise
-                var promise = $.ajax({
-                    type: action,
-                    url: url,
-                    data: JSON.stringify(payload),
-                    contentType: 'application/json; charset=utf-8',
-                    dataType: 'json'
-                });
-
-                return promise;*/
+            
             }
         });
     };
