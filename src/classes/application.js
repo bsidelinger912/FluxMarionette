fedfaa8004570bc8164a71c1b78327e7cf370aaa
unknown <sidb235@LNIDPC003109D.wads.res>
2015-07-31 10:33:37 -0700

    added dispatcherEvents and wait for to Application class

--- src/classes/application.js
+++ src/classes/application.js
@@ -2,13 +2,16 @@ define(function (require) {
 	var Marionette = require('backbone.marionette');
 	
 	//mixins here
-	var dispatcher = require('mixins/dispatcher');
+	var dispatcher = require('mixins/dispatcher'),
+		api = require('mixins/api'),
+		waitFor = require('mixins/waitFor'),
+		dispatcherEvents = require('mixins/dispatcherEvents');
 
 	//just adding the dispatcher for now
 	var application = function(options) {
 	    Marionette.Application.call(this, options);
 	};
-	_.extend(application.prototype, Marionette.Application.prototype, dispatcher);
+	_.extend(application.prototype, Marionette.Application.prototype, dispatcher, api, waitFor);
 	application.extend = Marionette.Application.extend;  
 
 	return application;
